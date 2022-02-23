169211f4792fbc716e7bdf09cc2b68084044a5f2
Duskin Ilya <ilya@buyr.com>
2022-02-23 15:27:04 +0500

    master

--- vars/notifyToSlack.groovy
+++ vars/notifyToSlack.groovy
@@ -1,15 +1,38 @@
-def call(String chanel, String message, Boolean disabled = false) {
-    if (!disabled) {
-        SLACK_COLOR_MAP = [
-                'SUCCESS': 'good',
-                'FAILURE': 'danger',
-                'UNSTABLE': 'danger',
-        ]
+def call(Map config) {
 
+    String chanel = config.get('chanel', null)
+    Boolean disabled = config.get('disabled', false).toBoolean()
+    String mesasge = config.get('message', generate_message())
+
+    RESULT_COLOR_MAP = [
+        'SUCCESS': 'good',
+        'FAILURE': 'danger',
+        'UNSTABLE': 'danger',
+    ]
+
+    if (!chanel) {
+        error("Missing argument: chanel")
+    }
+
+    if (!disabled) {
         slackSend(
             channel: chanel,
             message: message,
-            color: SLACK_COLOR_MAP[currentBuild.currentResult]
+            color: RESULT_COLOR_MAP[currentBuild.currentResult]
         )
     }
+}
+
+
+def generate_message() {
+    message_rows = [
+        "*${currentBuild.currentResult}:* ${BUILD_TAG} (<$BUILD_URL|Open>)",
+        "Duration: ${currentBuild.durationString}",
+        "Branch: ${env.BRANCH}",
+        "Test environment: ${env.TEST_ENVIRONMENT}",
+        "Test suit: ${env.SUITE_NAME}",
+        "See the detailed report <${env.BUILD_URL}/allure|here>",
+    ]
+
+    return message_rows.join("\n")
 }
